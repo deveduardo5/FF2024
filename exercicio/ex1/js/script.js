8d13450e46933455bff8bed3b4d043b8278c01e2
deveduardo5 <eduardo.asouza23@senacsp.edu.br>
2024-09-27 20:13:42 -0300

    feat: Ex 1 corrigido

---
+++ exercicio/ex1/js/script.js
@@ -0,0 +1,12 @@
+const texto = "Texto Corrido"
+let numeroInteiro = 10;
+const numeroDecimal = 10.5;
+
+// Forma 1
+console.log(texto + " , " + numeroInteiro + " , " + numeroDecimal)
+
+// Forma 2
+console.log(texto + ' , ' + numeroInteiro + ' , ' + numeroDecimal)
+
+// Forma 3
+console.log(`${texto} , ${numeroInteiro} , ${numeroDecimal}`);
